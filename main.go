99e1d9f21e96e8e517ba0f357a14b3ce28f23605
gevann <evann.guilhem@ynov.com>
2025-09-15 09:53:45 +0200

    main

--- main.go
+++ main.go
@@ -6,8 +6,39 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"character"
 )
+
+type Equipment struct {
+	Head  string
+	Torso string
+	Feet  string
+}
+
+type Character struct {
+	Name      string
+	Class     string
+	Level     int
+	MaxHP     int
+	HP        int
+	Inventory []string
+	Skills    []string
+	Gold      int
+	Equipment Equipment
+}
+
+func initCharacter(name string, class string, level int, maxHP int, currentHP int, inventory []string) Character {
+	return Character{
+		Name:      name,
+		Class:     class,
+		Level:     level,
+		MaxHP:     maxHP,
+		HP:        currentHP,
+		Inventory: inventory,
+		Skills:    []string{"Coup de poing"},
+		Gold:      100,
+		Equipment: Equipment{"", "", ""},
+	}
+}
 
 func displayInfo(c *Character) {
 	fmt.Println("\nInformations du personnage")
